98fbbee1f7158d2ba7538effbde71abe0ae9efc6
Ugur <hakan.ugur@sky.uk>
2019-04-02 23:55:23 +0100

    home work week 1

--- order-date.js
+++ order-date.js
@@ -1,3 +1,9 @@
+
+ var deliveryFees={
+   standard:0.0,
+   fast:2.99,
+   express:4.99
+ }
 function init(){
   var orderButton=document.getElementById('submitOrder')
   orderButton.addEventListener('click', (event) => orderSubmit(event));
@@ -7,13 +13,28 @@ function init(){
   amountBox.addEventListener('blur', (event) => calculateTotalAmount(amountBox, unitPriceBox));
   
   unitPriceBox.addEventListener('blur', (event)=> calculateTotalAmount(amountBox, unitPriceBox));
+  var radioButtons=document.getElementsByName('delivery');
+  radioButtons.forEach(radioButton=>{
+    radioButton.addEventListener('click', (event) =>  addDeliveryFeeToTotal(event.target.value));
+  });
+
+  var itemBox=document.getElementById('itemName');
+  itemBox.addEventListener('change', (event) =>  itemSelected(event.target.value));
+
+  document.getElementById('unitPrice').value=document.getElementById('itemName').value;
+}
+
+function itemSelected(val){
+  document.getElementById('unitPrice').value=val;
 }
 
 function orderSubmit(event){
     var orderDateValidation=document.getElementById('invalidOrderDate');
     var dateValue=document.getElementById('orderDate').value;
-    if(!validateDate(dateValue)){
+    if(!isValidOrderDate(dateValue)){
       orderDateValidation.style.display='block';  
+    }else{
+      orderDateValidation.style.display='none'; 
     }   
 }
 
@@ -24,27 +45,50 @@ function calculateTotalAmount(amountBox, unitPriceBox){
    totalPriceBox.value=amountBox.value * unitPriceBox.value;
 }
 
-function validateDate(date){
-  /*validation code :
-    - Date will be validated on date input blur 
-    check if the date is valid date, 
-    for example: 32/03/2019 is invalid date
-                 30/02/2019 is invalid date
-    if the date is before the current date, it is invalid date 
-  */
-  return false;
-}
-
 function addDeliveryFeeToTotal(deliveryType){
-  /*
-    standard ==> 0 add,
-    fast ==> 2.99 add,
-    express => 4.99 add
-  */
+  var totalAll=document.getElementById('totalAll');
+  var totalPriceBox=document.getElementById('totalPrice');
+  totalAll.value=(Number(totalPriceBox.value) + Number(deliveryFees[deliveryType])).toFixed(2);
 }
 
 init();
 
+
+
+function isValidOrderDate(date){
+  return isValidDate(date) && !isPastDate(date);
+}
+function isValidDate(date){
+ var dateParts=date.split('/');
+ var day=dateParts[0];
+ var month=dateParts[1];
+ var year=dateParts[2];
+ var daysOfMonth=[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+ if(dateParts.length !== 3)
+    return false;
+ if(!isNumeric(day) || !isNumeric(month) || !isNumeric(year))
+    return false;
+ if(month > 12)
+    return false;
+ if(month==='02' && year % 4 === 0){
+      if(day > 29)
+          return false;
+ }
+ else if(day > daysOfMonth[month -1]){
+       return false;
+ }
+ return true;
+}
+
+function isPastDate(year, month, day){
+  var today=new Date();
+  var givenDate=new Date(year, month - 1, day);
+  return givenDate < today;
+}
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
 
 /*
   homework:
--- playground/date.js
+++ playground/date.js
@@ -1,19 +1,54 @@
 
+function isValidOrderDate(date){
+    return isValidDate(date) && !isPastDate(date);
+}
 function isValidDate(date){
-   
+   var dateParts=date.split('/');
+   var day=dateParts[0];
+   var month=dateParts[1];
+   var year=dateParts[2];
+   var daysOfMonth=[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+   if(dateParts.length !== 3)
+      return false;
+   if(!isNumeric(day) || !isNumeric(month) || !isNumeric(year))
+      return false;
+   if(month > 12)
+      return false;
+   if(month==='02' && year % 4 === 0){
+        if(day > 29)
+            return false;
+   }
+   else if(day > daysOfMonth[month -1]){
+         return false;
+   }
+   return true;
 }
 
+function isPastDate(year, month, day){
+    var today=new Date();
+    var givenDate=new Date(year, month - 1, day);
+    return givenDate < today;
+}
 
 function isNumeric(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
-  }
+}
 
 
 
-
- var result= isValidDate('01/13/2019');
+ var result= isValidOrderDate('01/04/2019');
  console.log(result);
+ //isValidDate('fdjsfkljsd) ==> false
 //date(aa/11/2019) ==>false
 //date(01/13/2019) ==>false
 //date(32/01/2019) ==>false
+//date(29/02/2020) ==> true
 //date()
+
+
+
+
+
+
+
+
