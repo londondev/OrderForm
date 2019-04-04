00d3b44b96ac9fea7435fa65f5c8244463b60a73
ahmet-serdar <nerelerdeyim505@gmail.com>
2019-04-05 01:18:41 +0200

    asb changes week 2

--- order-date.js
+++ order-date.js
@@ -1,6 +1,11 @@
+var deliveryFees = {
+  standard: 0.0,
+  fast: 2.99,
+  express: 4.99
+};
+var orderList = [];
 function init() {
   var orderButton = document.getElementById("submitOrder");
-
   orderButton.addEventListener("click", event => orderSubmit(event));
 
   var amountBox = document.getElementById("amount");
@@ -12,36 +17,62 @@ function init() {
   unitPriceBox.addEventListener("blur", event =>
     calculateTotalAmount(amountBox, unitPriceBox)
   );
+  var radioButtons = document.getElementsByName("delivery");
+  radioButtons.forEach(radioButton => {
+    radioButton.addEventListener("click", event =>
+      addDeliveryFeeToTotal(event.target.value)
+    );
+  });
 
-  var deliveryFeeList = document.getElementsByName("delivery");
-
-  deliveryFeeList.forEach(x =>
-    document
-      .getElementById(x.id)
-      .addEventListener("click", addDeliveryFeeToTotal)
-  );
+  var itemBox = document.getElementById("itemName");
+  itemBox.addEventListener("change", event => itemSelected(event.target.value));
 
   var orderDateBox = document.getElementById("orderDate");
-  orderDateBox.addEventListener("blur", event => orderSubmit(event));
-
-  var itemName = document.getElementById("itemN");
-  itemName.addEventListener("click", event => getItemPrice(event));
+  orderDateBox.addEventListener("blur", event => orderDate(event));
 }
 
-function getItemPrice(event) {
-  var itemPrice = document.getElementById("itemN").value;
-  var unitPriceBox = document.getElementById("unitPrice");
-  unitPriceBox.value = itemPrice;
+function itemSelected(val) {
+  document.getElementById("unitPrice").value = val;
 }
 
-function orderSubmit(event) {
+function orderDate(event) {
   var orderDateValidation = document.getElementById("invalidOrderDate");
   var dateValue = document.getElementById("orderDate").value;
-  if (!validateDate(dateValue)) {
+  if (!isValidOrderDate(dateValue)) {
     orderDateValidation.style.display = "block";
   } else {
     orderDateValidation.style.display = "none";
   }
+}
+
+function orderSubmit(event) {
+  var orderDate = document.getElementById("orderDate").value;
+  var itemName = document.getElementById("itemName").value;
+  var amount = document.getElementById("amount").value;
+  var unitPrice = document.getElementById("unitPrice").value;
+  var totalPrice = document.getElementById("totalPrice").value;
+  var totalAll = document.getElementById("totalAll").value;
+  var deliveryFee = (Number(totalAll) - Number(totalPrice)).toFixed(2);
+
+  console.log("order date " + orderDate);
+  console.log("itemName " + itemName);
+  console.log("amount " + amount);
+  console.log("unitPrice " + unitPrice);
+  console.log("totalPrice " + totalPrice);
+  console.log("totalAll " + totalAll);
+  console.log("fee " + deliveryFee);
+
+  createNewOrder(
+    orderDate,
+    itemName,
+    amount,
+    unitPrice,
+    totalPrice,
+    deliveryFee,
+    totalAll
+  );
+  console.log(orderList);
+  resetOrderForm();
 }
 
 function calculateTotalAmount(amountBox, unitPriceBox) {
@@ -50,89 +81,99 @@ function calculateTotalAmount(amountBox, unitPriceBox) {
   totalPriceBox.value = amountBox.value * unitPriceBox.value;
 }
 
-function validateDate(date) {
-  var checkBoxArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "/", "9"];
-  var today = new Date();
-  console.log(today);
-  var dd = today.getDate();
-  var mm = today.getMonth() + 1; //As January is 0.
-  var yyyy = today.getFullYear();
-  let arr1 = date.split("");
-
-  let arr = date.split("/");
-  let day = Number(arr[0]);
-  let month = Number(arr[1]);
-  let year = Number(arr[2]);
-  let listOfDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-  for (var i = 0; i < arr1.length; i++) {
-    if (!checkBoxArr.includes(arr1[i])) {
-      return false;
-    }
-  }
-  if (arr1.length !== 10) {
-    return false;
-  } else if (year < yyyy) {
-    return false;
-  } else if (year === yyyy && month < mm) {
-    return false;
-  } else if (year === yyyy && month === mm && day <= dd) {
-    return false;
-  } else if (month > 12) {
-    return false;
-  } else if (month === 1 || month > 2) {
-    if (day > listOfDays[month - 1]) {
-      return false;
-    }
-  } else if (month === 2) {
-    if (year % 4 === 0 && day > 29) {
-      return false;
-    } else if (year % 4 !== 0 && day > listOfDays[month - 1]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-function addDeliveryFeeToTotal() {
-  var amountBox = document.getElementById("amount");
-  var unitPriceBox = document.getElementById("unitPrice");
-  if (!amountBox.value || !unitPriceBox.value) return;
-  var totalPriceBox = document.getElementById("totalPrice").value;
+function addDeliveryFeeToTotal(deliveryType) {
   var totalAll = document.getElementById("totalAll");
-  var type = document.getElementsByName("delivery");
-  var deliveryAmount = 0;
-  if (type[0].checked) {
-    deliveryAmount = 0;
-  } else if (type[1].checked) {
-    deliveryAmount = 2.99;
-  } else {
-    deliveryAmount = 4.99;
-  }
-  totalAll.value = (deliveryAmount + Number(totalPriceBox)).toFixed(2);
+  var totalPriceBox = document.getElementById("totalPrice");
+  totalAll.value = (
+    Number(totalPriceBox.value) + Number(deliveryFees[deliveryType])
+  ).toFixed(2);
 }
 
 init();
 
-/*validation code :
-    - Date will be validated on date input blur
-    check if the date is valid date,
-    for example: 32/03/2019 is invalid date
-                 30/02/2019 is invalid date
-    if the date is before the current date, it is invalid date
-  */
+function isValidOrderDate(date) {
+  return isValidDate(date) && !isPastDate(date);
+}
+function isValidDate(date) {
+  var dateParts = date.split("/");
+  var day = dateParts[0];
+  var month = dateParts[1];
+  var year = dateParts[2];
+  var daysOfMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  if (dateParts.length !== 3) return false;
+  if (!isNumeric(day) || !isNumeric(month) || !isNumeric(year)) return false;
+  if (month > 12) return false;
+  if (month === "02" && year % 4 === 0) {
+    if (day > 29) return false;
+  } else if (day > daysOfMonth[month - 1]) {
+    return false;
+  }
+  return true;
+}
 
-/*
-===
-    standard ==> 0 add,
-    fast ==> 2.99 add,
-    express => 4.99 add
-  */
+function isPastDate(year, month, day) {
+  var today = new Date();
+  var givenDate = new Date(year, month - 1, day);
+  return givenDate < today;
+}
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+var SaveOrderObject = function(
+  date,
+  itemName,
+  amount,
+  unitPrice,
+  total,
+  deliveryFee,
+  deliveryTotal
+) {
+  this.orderDate = date;
+  this.itemName = itemName;
+  this.orderAmount = amount;
+  this.unitPrice = unitPrice;
+  this.total = total;
+  this.deliveryFee = deliveryFee;
+  this.totalIncludeDelivery = deliveryTotal;
+};
+
+function createNewOrder(
+  date,
+  itemName,
+  amount,
+  unitPrice,
+  total,
+  deliveryFee,
+  deliveryTotal
+) {
+  var newOrder = new SaveOrderObject(
+    date,
+    itemName,
+    amount,
+    unitPrice,
+    total,
+    deliveryFee,
+    deliveryTotal
+  );
+  orderList.push(newOrder);
+}
+
+function resetOrderForm() {
+  document.getElementById("orderDate").value = "";
+  document.getElementById("itemName").value = "";
+  document.getElementById("amount").value = "";
+  document.getElementById("unitPrice").value = "";
+  document.getElementById("totalPrice").value = "";
+  document.getElementById("totalAll").value = "";
+  var radioButtons = document.getElementsByName("delivery");
+  radioButtons.forEach(radioButton => (radioButton.checked = false));
+}
 
 /*
   homework:
-  1- Implement validateDate
-  2- Implement addDeliveryFeeToTotal
-  3- Add items to array with each price, when item is selected
-  populate, unit price automatically.
+ 1- On submit, save order to array,
+ 2- Clean order form
+ 3- Order date must be validated on blur
 */
