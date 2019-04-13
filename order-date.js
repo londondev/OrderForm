16ba794b0e10f17c810af235de7ed0a3dfa0e2cd
ahmet-serdar <nerelerdeyim505@gmail.com>
2019-04-13 10:42:01 +0200

    fourth changes

--- order-date.js
+++ order-date.js
@@ -5,6 +5,8 @@ var deliveryFees = {
 };
 var orderList = [];
 var selectedDeliveryType;
+var inputFilled = false;
+
 function init() {
   var orderDateInput = document.getElementById("orderDate");
   orderDateInput.addEventListener("blur", validateOrderDate);
@@ -17,6 +19,7 @@ function init() {
   amountBox.addEventListener("blur", event =>
     calculateTotalAmount(amountBox, unitPriceBox)
   );
+
   unitPriceBox.addEventListener("blur", event =>
     calculateTotalAmount(amountBox, unitPriceBox)
   );
@@ -44,21 +47,39 @@ function init() {
 
 function itemSelected(val) {
   document.getElementById("unitPrice").value = val;
+
+  var amountBox = document.getElementById("amount");
+  var unitPriceBox = document.getElementById("unitPrice");
+  calculateTotalAmount(amountBox, unitPriceBox);
+
+  addDeliveryFeeToTotal(selectedDeliveryType);
 }
 
 function orderSubmit() {
-  var newOrder = {
-    orderDate: document.getElementById("orderDate").value,
-    itemName: document.getElementById("itemName").value,
-    amount: document.getElementById("amount").value,
-    unitPrice: document.getElementById("unitPrice").value,
-    totalPrice: document.getElementById("totalPrice").value,
-    selectedDeliveryType,
-    totalAll: document.getElementById("totalAll").value
-  };
-  orderList.push(newOrder);
-  cleanForm();
-  renderRow(newOrder);
+  isPopulated();
+  if (inputFilled) {
+    var newOrder = {
+      orderId:
+        orderList.length === 0
+          ? 1
+          : Math.max.apply(
+              Math,
+              orderList.map(function(o) {
+                return o.orderId;
+              })
+            ) + 1,
+      orderDate: document.getElementById("orderDate").value,
+      itemName: document.getElementById("itemName").value,
+      amount: document.getElementById("amount").value,
+      unitPrice: document.getElementById("unitPrice").value,
+      totalPrice: document.getElementById("totalPrice").value,
+      selectedDeliveryType,
+      totalAll: document.getElementById("totalAll").value
+    };
+    orderList.push(newOrder);
+    cleanForm();
+    renderRow(newOrder);
+  }
 }
 
 function cleanForm() {
@@ -67,8 +88,8 @@ function cleanForm() {
     (document.getElementById("amount").value = ""),
     (document.getElementById("unitPrice").value = ""),
     (document.getElementById("totalPrice").value = ""),
-    (document.getElementById("totalAll").value = ""),
-    (document.getElementsByName("delivery")[0].checked = true);
+    (selectedDeliveryType = document.getElementsByName("delivery")[0].checked = true),
+    (document.getElementById("totalAll").value = "");
 }
 
 function renderRow(newOrder) {
@@ -82,6 +103,54 @@ function renderRow(newOrder) {
     currentCell.innerHTML = val;
     cellIndex++;
   });
+
+  currentCell = row.insertCell(cellIndex++);
+  currentCell.innerHTML =
+    '<button id="btn_' +
+    newOrder.orderId +
+    '" onclick="deleteOrder(' +
+    newOrder.orderId +
+    ')">Delete</button>';
+  currentCell = row.insertCell(cellIndex++);
+  currentCell.innerHTML =
+    '<button id="btn_' +
+    newOrder.orderId +
+    '" onclick="editOrder(' +
+    newOrder.orderId +
+    ')">Edit</button>';
+}
+
+
+
+function deleteOrder(orderId) {
+  orderList = orderList.filter(o => o.orderId !== orderId);
+  var btn = document.getElementById("btn_" + orderId);
+  deleteRow(btn);
+}
+
+function deleteRow(btn) {
+  var row = btn.parentNode.parentNode;
+  row.parentNode.removeChild(row);
+}
+
+function isPopulated() {
+  var emptyOrderDate = document.getElementById("emptyOrderDate");
+  var dateValue = document.getElementById("orderDate").value;
+ 
+  var orderAmount = document.getElementById("orderAmount");
+  var amountValue = document.getElementById("amount").value
+  if (!dateValue) {
+    emptyOrderDate.style.display = "block";
+  } else {
+    emptyOrderDate.style.display = "none";
+  }
+  
+  if(!amountValue) {
+    orderAmount.style.display = "block";
+  } else {
+     orderAmount.style.display = "none";
+     inputFilled = true;
+  }  
 }
 
 function validateOrderDate() {
@@ -113,7 +182,11 @@ function addDeliveryFeeToTotal(deliveryType) {
 init();
 
 function isValidOrderDate(date) {
-  return isValidDate(date) && !isPastDate(date);
+  var dateParts = date.split("/");
+  var day = dateParts[0];
+  var month = dateParts[1];
+  var year = dateParts[2];
+  return isValidDate(date) && !isPastDate(year, month, day);
 }
 function isValidDate(date) {
   var dateParts = date.split("/");
@@ -150,3 +223,25 @@ function createNewOrder() {
 
   orderList.push(newOrder);
 }
+function editOrder(orderId) {
+  var btn = document.getElementById("btn_" + orderId);
+  var row = btn.parentNode.parentNode;
+  var arr = row.cells[2].textContent;
+
+  console.log(arr);
+  document.getElementById("orderDate").value = row.cells[1].textContent;
+  document.getElementById("itemName").value = row.cells[2].textContent;
+  document.getElementById("amount").value = row.cells[3].textContent;
+  document.getElementById("unitPrice").value = row.cells[4].textContent;
+  document.getElementById("totalPrice").value = row.cells[5].textContent;
+  document.getElementById("totalAll").value = row.cells[7].textContent;
+  selectedDeliveryType = row.cells[6].textContent;
+  
+  deleteOrder(orderId);
+}
+/*
+  homework:
+ 1- On submit, save order to array,
+ 2- Clean order form
+ 3- Order date must be validated on blur
+*/
